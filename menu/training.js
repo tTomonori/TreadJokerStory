af840c12bc3fc505c9daa3588b5a6a18bee88b82
momiiyuya <u_sunset@ezweb.ne.jp>
2017-11-21 15:24:13 +0900

    メニュー画面

--- menu/training.js
+++ menu/training.js
@@ -1,5 +1,9 @@
-
+	//選択肢を動的に生成
+	var list = $('<div class="training-chara" id="training-chara-list"></div>');
+	list.append('<div class="inselectable"></div>')
 	for (var i = 0; i < AllCharaNum; i++) {
 		var div = $("<div class='training-chara-list'>"+i+"番目のキャラ</div>");
-		$("#training-menu .menu-left .menu-list")[0].prepend(div[0]);
+		changeCss($(div));
+		$(list).append(div);
 	}
+	$("#training-menu .menu-left .menu-list")[0].prepend(list[0]);
